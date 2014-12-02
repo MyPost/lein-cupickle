4926dcd0c4dbca21333b142ec20f02dd75b8bdf2
Lyndon Maydwell <lyndon@sordina.net>
2014-12-02 15:23:35 +1100

    Version bump due to dependency bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject au.com.auspost/lein-cupickle "0.3.0"
+(defproject au.com.auspost/lein-cupickle "0.4.0"
   :description "A lein plugin to run cucumber tests based on the cupickle library."
   :dependencies [[au.com.auspost/cupickle "0.2.0"]]
   :url "https://github.com/MyPost/lein-cupickle"
